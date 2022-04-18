d2484fbc357f94f6162677d3bf59e9cf323d62f4
framvaq <frv056@inlumine.ual.es>
2022-04-18 03:46:18 +0200

    fin practica2

---
+++ practica_2/sources/practica_2/FileManager.java
@@ -0,0 +1,10 @@
+package practica_2;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileManager {
+    protected static String path = System.getProperty("user.dir") + System.getProperty("file.separator") + "practica_2"
+            + System.getProperty("file.separator") + "assets" + System.getProperty("file.separator");
+
+}
